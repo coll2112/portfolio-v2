d58dda23e9925b1aa177e3c5bbd277fa4b46e549
Gray754 <lgc9312@gmail.com>
2019-05-16 00:14:57 -0500

    Added firebase for hosting. Updated responsive design for Navbar. Still in very early concept.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ class Navbar extends Component {
     super();
     this.state = {
       isToggled: false,
-      mobileNavMenu: 'mobileNavContainer slideInRight'
+      mobileNavMenu: 'mobileNavbarContainer animated slideInLeft'
     };
   }
 
@@ -16,98 +16,127 @@ class Navbar extends Component {
   };
 
   render() {
-    // console.log(this.state.isToggled);
+    console.log(this.state.isToggled);
     return (
-      <div className='navbarContainer'>
-        <span
-          className={this.state.mobileNavMenu}
-          onClick={() => this.toggleNav()}
-        >
-          <i className='fas fa-bars' />
-        </span>
-        <div className='navbarContent'>
-          <img src={Selfie} />
-          <div>
-            <a href='#home'>
-              <p>
-                <i className='fas fa-home' />
-                <span className='linkTitle'>Home</span>
-              </p>
-            </a>
-            <a href='#about'>
-              <p>
-                <i className='fas fa-user' />
-                <span className='linkTitle'>About</span>
-              </p>
-            </a>
-            <a href='#portfolio'>
-              <p>
-                <i className='fas fa-image' />
-                <span className='linkTitle'>Portfolio</span>
-              </p>
-            </a>
-            <a href='#skills'>
-              <p>
-                <i class='fas fa-code' />
-                <span className='linkTitle'>Skills</span>
-              </p>
-            </a>
-            <a href='#contact'>
-              <p>
-                <i className='fas fa-envelope' />
-                <span className='linkTitle'>Contact</span>
-              </p>
-            </a>
-          </div>
-          <div className='social'>
-            <a href='https://github.com/Gray754' target='_blank'>
-              <p>
-                <i className='fab fa-github' />
-              </p>
-            </a>
-            <a href='https://www.linkedin.com/in/leegcollins/' target='_blank'>
-              <p>
-                <i className='fab fa-linkedin' />
-              </p>
-            </a>
+      <div>
+        <div className='mobileNavBar'>
+          <span>
+            <i className='fas fa-bars' onClick={() => this.toggleNav()} />
+          </span>
+        </div>
+        <div className='navbarContainer'>
+          <div className='navbarContent'>
+            <img src={Selfie} />
+            <div>
+              <a href='#home'>
+                <p>
+                  <i className='fas fa-home' />
+                  <span className='linkTitle'>Home</span>
+                </p>
+              </a>
+              <a href='#about'>
+                <p>
+                  <i className='fas fa-user' />
+                  <span className='linkTitle'>About</span>
+                </p>
+              </a>
+              <a href='#portfolio'>
+                <p>
+                  <i className='fas fa-image' />
+                  <span className='linkTitle'>Portfolio</span>
+                </p>
+              </a>
+              <a href='#skills'>
+                <p>
+                  <i class='fas fa-code' />
+                  <span className='linkTitle'>Skills</span>
+                </p>
+              </a>
+              <a href='#contact'>
+                <p>
+                  <i className='fas fa-envelope' />
+                  <span className='linkTitle'>Contact</span>
+                </p>
+              </a>
+            </div>
+            <div className='social'>
+              <a href='https://github.com/Gray754' target='_blank'>
+                <p>
+                  <i className='fab fa-github' />
+                </p>
+              </a>
+              <a
+                href='https://www.linkedin.com/in/leegcollins/'
+                target='_blank'
+              >
+                <p>
+                  <i className='fab fa-linkedin' />
+                </p>
+              </a>
+            </div>
           </div>
         </div>
-        {/* <div className='mobileNavContent'>
-          <img src={Selfie} />
-          <div>
-            <p>
-              <i className='fas fa-home' title='Home' />
-              <p>Home</p>
-            </p>
-            <p>
-              <i className='fas fa-user' title='About' />
-              <p>About</p>
-            </p>
-            <p>
-              <i className='fas fa-image' title='Portfolio' />
-              <p>Portfolio</p>
-            </p>
-            <p>
-              <i className='fas fa-laptop-code' title='Skills' />
-              <p>Skills</p>
-            </p>
-            <p>
-              <i className='fas fa-envelope' title='Contact' />
-              <p>Contact</p>
-            </p>
+        {this.state.isToggled ? (
+          <div
+            className={this.state.mobileNavMenu}
+            onBlur={() =>
+              this.setState({
+                isToggled: false,
+              })
+            }
+          >
+            <div className='mobileNavbarContent'>
+              <img src={Selfie} />
+              <div>
+                <a href='#home'>
+                  <p>
+                    <i className='fas fa-home' />
+                    <span className='linkTitle'>Home</span>
+                  </p>
+                </a>
+                <a href='#about'>
+                  <p>
+                    <i className='fas fa-user' />
+                    <span className='linkTitle'>About</span>
+                  </p>
+                </a>
+                <a href='#portfolio'>
+                  <p>
+                    <i className='fas fa-image' />
+                    <span className='linkTitle'>Portfolio</span>
+                  </p>
+                </a>
+                <a href='#skills'>
+                  <p>
+                    <i class='fas fa-code' />
+                    <span className='linkTitle'>Skills</span>
+                  </p>
+                </a>
+                <a href='#contact'>
+                  <p>
+                    <i className='fas fa-envelope' />
+                    <span className='linkTitle'>Contact</span>
+                  </p>
+                </a>
+              </div>
+              <div className='social'>
+                <a href='https://github.com/Gray754' target='_blank'>
+                  <p>
+                    <i className='fab fa-github' />
+                  </p>
+                </a>
+                <a
+                  href='https://www.linkedin.com/in/leegcollins/'
+                  target='_blank'
+                >
+                  <p>
+                    <i className='fab fa-linkedin' />
+                  </p>
+                </a>
+              </div>
+            </div>
           </div>
-          <div className='social'>
-            <p>
-              <i className='fab fa-github' />
-            </p>
-            <p>
-              <i className='fab fa-linkedin' />
-            </p>
-            <p>
-              <i className='fas fa-at' />
-            </p>
-          </div>
-        </div> */}
+        ) : null}
       </div>
     );
   }
